5586cf3308717cfa9a0cf8fe75b24065f9f2bfed
gabbybeall <gabriellebeall@gmail.com>
2026-05-23 13:24:32 -0700

    Added GitHub repo URL

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,24 @@ import heroImg from "./assets/hero.png";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://gabrielle-b.netlify.app/" target="_blank">
+          Gabby Beall{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/gabbybeall/weather-application-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
