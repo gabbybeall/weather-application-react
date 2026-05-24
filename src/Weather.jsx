d6a72ac50b22bdd1b7f68d2e196aee7182a7da65
gabbybeall <gabriellebeall@gmail.com>
2026-05-24 13:39:58 -0700

    Added basic styling

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -11,32 +11,37 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
       <h1>San Francisco</h1>
-      <div className="row">
+      <ul>
+        <li>Saturday 1:00 PM</li>
+        <li>Partly Sunny</li>
+      </ul>
+      <div className="row mt-3">
         <div className="col-6">
           <img
             src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
             alt="Partly Sunny"
           />
-          62°F
+          <span className="temperature">62</span>
+          <span className="unit">°F</span>
+        </div>
+        <div className="col-6">
           <ul>
             <li>Precipitation: 5%</li>
             <li>Humidity: 74%</li>
             <li>Wind: 13 mph</li>
-          </ul>
-        </div>
-        <div className="col-6">
-          Weather
-          <ul>
-            <li>Saturday 1:00 PM</li>
-            <li>Partly Sunny</li>
           </ul>
         </div>
       </div>
